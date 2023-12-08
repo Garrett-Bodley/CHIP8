e16c2346c597ea248cd5fe859e24c728fdeaa99c
Garrett Bodley <garrett.bodley@gmail.com>
2023-12-08 15:49:44 -0500

    Reads file and loads it into a buffer

--- chip8.c
+++ chip8.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 
 #define MEM_SIZE 4096
 #define REGISTER_COUNT 16
@@ -16,7 +18,69 @@ uint8_t SOUND_TIMER;
 uint8_t MEMORY[MEM_SIZE];
 uint8_t SCREEN[SCREEN_REGISTER_COUNT];
 
-int main()
-{
+typedef struct ROM {
+  u_int8_t* buffer;
+  size_t length;
+} ROM;
 
+ROM load_file(char *path)
+{
+  FILE *file;
+  int file_size;
+  size_t result;
+
+  file = fopen(path, "rb");
+
+  if (file == NULL)
+  {
+    perror("Error when opening file");
+    exit(1);
+  }
+
+  fseek(file, 0, SEEK_END);
+  file_size = ftell(file);
+  rewind(file);
+
+  ROM rom;
+  rom.buffer = malloc(file_size);
+  if (rom.buffer == NULL)
+  {
+    perror("Memory allocation error");
+    fclose(file);
+    exit(2);
+  }
+
+  result = fread(rom.buffer, 1, file_size, file);
+  if (result != file_size)
+  {
+    perror("Error when reading file");
+    fclose(file);
+    free(rom.buffer);
+    exit(3);
+  }
+
+  rom.length = file_size;
+  fclose(file);
+  return rom;
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc < 2)
+  {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
+
+  ROM rom = load_file(argv[1]);
+
+  printf("Rom Length: %zu\n", rom.length);
+  for(int i = 0; i < rom.length; i++){
+    if(i % 16 == 0){ printf("\n"); }
+    if(i % 8 == 0){ printf(" "); }
+    printf("%02x ", rom.buffer[i]);
+  }
+  printf("\n");
+  return 0;
+  
 }
