04e9ba757a532bd33ab38aaa80dfc19858ddb707
Garrett Bodley <garrett.bodley@gmail.com>
2024-03-01 01:51:48 -0500

    Mid implementation of DRAW instruction.
    Seem to be running into a weird bug with the draw instruction. There's a
    lot of complicated logic inside of it, and I'm unsure if the problem is
    in the draw logic, or if previous instructions may be leading to
    cascading errors.
    
    Considering writing some tests for the prior instructions to ensure they
    work properly. Committing now to backup the in-progress work. May squash
    this commit at a later date.

--- chip8.c
+++ chip8.c
@@ -7,6 +7,9 @@
 #define STACK_SIZE 16
 #define SCREEN_REGISTER_COUNT 256 // 64 * 32 / 8
 
+#define SCREEN_HEIGHT 32
+#define SCREEN_WIDTH 64
+
 uint8_t SP;
 uint16_t I;
 uint16_t PC;
@@ -73,6 +76,19 @@ ROM load_file(char *path)
   return rom;
 }
 
+void log_screen(){
+  for(int i = 0; i < SCREEN_HEIGHT; i++){
+    int flag = 0;
+    for(int j = 0; j < SCREEN_WIDTH / 8; j++){
+      int mem_offset = j + (i * 8);
+      printf("%02x", SCREEN[mem_offset]);
+      if(flag != 0 ){ printf(" "); }
+      flag = ~flag;
+    }
+    printf("\n");
+  }
+}
+
 void load_font()
 {
   uint8_t font[16][5] = {
@@ -118,12 +134,19 @@ void sys_init()
   DELAY_TIMER = 0;
   SOUND_TIMER = 0;
   load_font();
+
+  for(int i = 0; i < SCREEN_REGISTER_COUNT; i++){
+    SCREEN[i] = 0;
+  }
+
 }
 
-void fetch(Instruction* cur, u_int16_t PC, ROM rom)
+void fetch(Instruction* cur, ROM rom)
 {
   uint8_t first = rom.buffer[PC];
   uint8_t second = rom.buffer[PC + 1];
+
+  PC += 2;
 
   uint8_t first_nibble_mask = 0xf0;
   uint8_t first_nibble = (first & first_nibble_mask) >> 4;
@@ -157,8 +180,9 @@ void ret()
 
 void invalid_instruction(Instruction* cur)
 {
-  perror('Invalid instruction provided');
-  printf("Instruction: %16x\n", cur->instruction);
+  printf("Invalid instruction provided\n");
+  printf("Instruction: %04x\n", cur->instruction);
+  printf("PC: %i\n", PC - 1);
   exit(4);
 }
 
@@ -171,7 +195,7 @@ void JP(Instruction* cur)
   PC = cur->instruction & 0x0fff;
 }
 
-void jump_sys_address(cur)
+void jump_sys_addr(Instruction* cur)
 {
   // Jump to a machine code routine at nnn.
 
@@ -232,6 +256,108 @@ void LD_I(Instruction* cur)
   I = cur->instruction & 0x0fff;
 }
 
+void DRW_Vx_Vy_N(Instruction* cur)
+{
+  // Dxyn - DRW Vx, Vy, nibble
+  // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+
+  // The interpreter reads n bytes from memory, starting at the address stored in I.
+  // These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
+  // Sprites are XORed onto the existing screen. If this causes any pixels to be erased,
+  // VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is
+  // outside the coordinates of the display, it wraps around to the opposite side of the screen.
+
+  // See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more
+  // information on the Chip-8 screen and sprites.
+  uint8_t Vx = cur->first & 0x0f;
+  uint8_t Vy = (cur->second & 0xf0) >> 4;
+  uint8_t N = cur->second & 0x0f;
+
+  uint8_t x = REGISTERS[Vx] & 63;
+  uint8_t y = REGISTERS[Vy] & 31;
+
+  printf("DRAWING\n");
+  printf("Instruction: %04x\n", cur->instruction);
+  printf("N: %i\n", N);
+  printf("I: %04x\n", I);
+  printf("I contents: %02x\n", MEMORY[I]);
+  for(int i = 0; i < N; i++)
+  {
+    printf("  %02x\n", MEMORY[I + i]);
+  }
+  printf("\n");
+
+  log_screen();
+
+  REGISTERS[0xF] = 0;
+
+  // 64 x 32
+  int x_mem_offset = x / 8;
+  int shift_offset = x & 7;
+  for(int i = 0; i < N; i++)
+  {
+    int y_mem_offset = (y + i) * SCREEN_WIDTH / 8;
+    int mem_offset = x_mem_offset + y_mem_offset;
+    int sprite_word = MEMORY[I + i];
+
+    if(shift_offset == 0)
+    {
+      if(REGISTERS[0xF] == 0)
+      {
+        int mask_res = sprite_word & SCREEN[mem_offset];
+        if(mask_res != 0){ REGISTERS[0xF] = 1; }
+      }
+      SCREEN[mem_offset] ^= sprite_word;
+    }else{
+      int left_mask = sprite_word >> shift_offset;
+      int right_mask = sprite_word << (8 - shift_offset);
+
+      if(x == 63){ right_mask = 0; }
+      if(REGISTERS[0xF] == 0)
+      {
+        // What if I'm on the edge of the screen? This will wrap in a flat array
+        int left_mask_res = left_mask & (SCREEN[mem_offset] >> shift_offset);
+        int right_mask_res = right_mask & (SCREEN[mem_offset + 1] << (8 - shift_offset));
+        if(left_mask_res != 0 || right_mask_res != 0){ REGISTERS[0xF] = 1; }
+      }
+      SCREEN[mem_offset] ^= left_mask;
+      SCREEN[mem_offset + 1] ^= right_mask;
+    }
+    printf("\n--------------------------------\n");
+    printf("VF: %i\n\n", REGISTERS[0xF]);
+    log_screen();
+    exit(1);
+
+  }
+
+  // TODO:
+  // Do math to figure out what SCREEN addresses (potentially multiple) I need to write to.
+  // Then do some bitwise stuff to figure out if I should set the VF register
+    // First I need to extract the bits of the pixels I'm writing to
+  //  Bitwise & with sprite value
+    // If > 0, then set VF = 1 (there are bits that match between values)
+
+  // This is complicated by the fact that the bits I need to set may exist in
+  //   multiple virtual registers
+
+  // Also I need to interface with SDL?
+
+  // for(u_int8_t nth_byte = 0; nth_byte < N; nth_byte++)
+  // {
+  //   uint8_t sprite_row = MEMORY[I + nth_byte];
+
+  //   switch(REGISTERS[0xF])
+  //   {
+  //     case 0:
+  //       if(~sprite_row != SCREEN[])
+  //       MEMORY[I + nth_byte];
+  //       break;
+  //     case 1:
+  //       break;
+  //   }
+  // }
+}
+
 void decode(Instruction* cur)
 {
   switch(cur->first_nibble)
@@ -253,6 +379,9 @@ void decode(Instruction* cur)
     case 0xa:
       LD_I(cur);
       break;
+    case 0xd:
+      DRW_Vx_Vy_N(cur);
+      break;
 
   }
 }
@@ -273,12 +402,15 @@ int main(int argc, char* argv[])
 
   while(PC < rom.length)
   {
-    fetch(&cur, PC, rom);
-    printf("%04x\n", cur.instruction);
+    fetch(&cur, rom);
+    // printf("%04x\n", cur.instruction);
     decode(&cur);
-    PC += 2;
   }
 
+  // for(int i = 0; i < SCREEN_REGISTER_COUNT; i++){
+  //   printf("%04x ", SCREEN[i]);
+  //   printf("\n");
+  // }
   // printf("Rom Length: %zu\n", rom.length);
   // for(int i = 0; i < rom.length; i++){
   //   if(i % 16 == 0){ printf("\n"); }
@@ -297,4 +429,5 @@ int main(int argc, char* argv[])
   //   printf("%x ", MEMORY[0x50 + i]);
   // }
   // printf("\n");
+  printf("Yay!\n");
 }
