112b812994379686e6a39b590bd2212dc0b6fb51
Garrett Bodley <garrett.bodley@gmail.com>
2024-03-25 19:50:16 -0400

    Main now accounts for SDL and apple2 versions.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "../headers/chip8.h"
 
 int main(int argc, char* argv[])
 {
-  // printf("%s", "hello world?");
-  // #ifdef SDL
-  // if(argc < 2)
-  // {
-  //   printf("Usage: %s <filename>\n", argv[0]);
-  //   return 1;
-  // }
-  // chip8(argv[1]);
-  // #elif defined(APPLE2)
-  printf("Hello, Compiled CHIP-8\n");
-  // chip8();
-  // #endif
+  #ifdef SDL
+
+  if(argc < 2)
+  {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
+  chip8(argv[1]);
+
+  #elif defined(APPLE2)
+
+  chip8(NULL);
+  while(true);
+
+  #endif
 
   return 0;
 }
