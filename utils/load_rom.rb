d84f3758214e585d53892de549507f94b841cde2
Garrett Bodley <garrett.bodley@gmail.com>
2024-03-25 19:39:23 -0400

    Write ruby script to generate apple_rom.c

--- utils/load_rom.rb
+++ utils/load_rom.rb
@@ -1,25 +1,28 @@
 require 'pry-nav'
 require 'pathname'
 
-file_path = Pathname.new(File.expand_path('src/rom.c'))
+file_path = Pathname.new(File.expand_path('src/apple_rom.c'))
 
 rom = Pathname.new(File.expand_path('ROMs/IBM.ch8'))
 
-rom_file = File.open(rom, 'r')
+rom_file = File.open(rom, 'rb')
 
-mapped = rom_file.each_byte.map do |byte|
+mapped_bytes = rom_file.each_byte.map do |byte|
   "0x#{byte.to_s(16).rjust(2, '0')}"
 end
-bytes =  mapped.join(', ')
-
+bytes = mapped_bytes.join(', ')
 
 content = <<~CONTENT
+  // #{rom.to_s.sub(/.*?(ROMs\/.*)/, '\1')}
+
   #include <stdint.h>
+  #include <stddef.h>
 
-
-  uint8_t my_data[] = {
+  uint8_t rom[] = {
     #{bytes}
   };
+
+  size_t rom_size = #{mapped_bytes.size};
 CONTENT
 
 File.open(file_path, 'w+') do |file|
