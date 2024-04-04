616dfc6d0df09aa0334c4506d038dfc4a9d8bf79
Garrett Bodley <garrett.bodley@gmail.com>
2024-04-03 23:37:40 -0400

    Write test for OR_Vx_Vy ALU instruction.

--- tests/src/alu.c
+++ tests/src/alu.c
@@ -14,7 +14,7 @@ void setup(void)
   sys_init(&machine);
 }
 
-Test(Instruction, LD_Vx_Vy)
+Test(ALU, LD_Vx_Vy)
 {
   // 8xy0 - LD Vx, Vy
   // Set Vx = Vy.
@@ -38,4 +38,37 @@ Test(Instruction, LD_Vx_Vy)
 
   decode(&machine, &instruction);
   cr_expect(machine.REGISTERS[0xA] == 0x42, "Expected register 0xA to contain value 0x42, found %02x", machine.REGISTERS[0x0]);
+}
+
+Test(ALU, OR_Vx_Vy)
+{
+  // 8xy1 - OR Vx, Vy
+  // Set Vx = Vx OR Vy.
+
+  // Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the
+  // corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1.
+  // Otherwise, it is 0.
+
+  instruction[0] = 0x80;
+  instruction[1] = 0x11;
+  machine.REGISTERS[0x0] = 0x00;
+  machine.REGISTERS[0x1] = 0x01;
+  decode(&machine, &instruction);
+
+  cr_expect(machine.REGISTERS[0x0] == 0x01, "Expected REGSISTERS[0x0] to contain value 0x01, found %02x", machine.REGISTERS[0x0]);
+
+  instruction[0] = 0x8A;
+  instruction[1] = 0xB1;
+  machine.REGISTERS[0xA] = 0x02;
+  machine.REGISTERS[0xB] = 0x01;
+
+  cr_expect(machine.REGISTERS[0xA] == 0x03, "Expected REGISTERS[0xA] to contain value 0x03, found %02x", machine.REGISTERS[0xA]);
+
+  instruction[0] = 0x85;
+  instruction[1] = 0x61;
+  machine.REGISTERS[0x5] = 0x06;
+  machine.REGISTERS[0x6] = 0x02;
+
+  cr_expect(machine.REGISTERS[0x5] == 0x06, "Expected REGISTERS[0x5] to contain value 0x06, found %02x", machine.REGISTERS[0xA]);
+
 }
