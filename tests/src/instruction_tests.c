41ce577e8493fc45b6112631af6f2123ee324f96
Garrett Bodley <garrett.bodley@gmail.com>
2024-04-03 23:19:57 -0400

    Make LD_Vx_Vy test more thorough.

--- tests/src/instruction_tests.c
+++ tests/src/instruction_tests.c
@@ -125,15 +125,22 @@ Test(Instruction, LD_Vx_Vy)
 
   // Stores the value of register Vy in register Vx.
 
-  // Store REGISTERS[1] in REGISTERS[0]
+  // Set REGISTERS[0x0] = REGISTERS[0x1]
   instruction[0] = 0x80;
   instruction[1] = 0x10;
-
   // Set REGISTERS[1] to contain the value 0xFF
   machine.REGISTERS[0x01] = 0xFF;
 
   decode(&machine, &instruction);
-
   cr_expect(machine.REGISTERS[0x0] == 0xFF, "Expected register 0x0 to contain value 0xFF, found %02x", machine.REGISTERS[0x0]);
+
+  // Set REGISTERS[0x3] = REGISTERS[0xA]
+  instruction[0] = 0x83;
+  instruction[1] = 0xA0;
+  // Set REGISTERS[1] to contain the value 0xFF
+  machine.REGISTERS[0xA] = 0x42;
+
+  decode(&machine, &instruction);
+  cr_expect(machine.REGISTERS[0xA] == 0x42, "Expected register 0xA to contain value 0x42, found %02x", machine.REGISTERS[0x0]);
 }
 
