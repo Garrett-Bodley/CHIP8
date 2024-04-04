3f19d330858a58f6320e653b8e63db4a568ba5a3
Garrett Bodley <garrett.bodley@gmail.com>
2024-04-04 00:45:08 -0400

    Write AND_Vx_Vy test.

--- tests/src/alu.c
+++ tests/src/alu.c
@@ -74,3 +74,37 @@ Test(ALU, OR_Vx_Vy)
   cr_expect(machine.REGISTERS[0x5] == 0x06, "Expected REGISTERS[0x5] to contain value 0x06, found 0x%02x", machine.REGISTERS[0x5]);
 
 }
+
+Test(ALU, AND_Vx_Vy)
+{
+  // 8xy2 - AND Vx, Vy
+  // Set Vx = Vx AND Vy.
+
+  // Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the
+  // corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1.
+  // Otherwise, it is 0.
+
+  instruction[0] = 0x80;
+  instruction[1] = 0x12;
+  machine.REGISTERS[0x0] = 0x03;
+  machine.REGISTERS[0x1] = 0x01;
+  decode(&machine, &instruction);
+
+  cr_expect(machine.REGISTERS[0x0] == 0x01, "Expected REGSISTERS[0x0] to contain value 0x01, found 0x%02x", machine.REGISTERS[0x0]);
+
+  instruction[0] = 0x8A;
+  instruction[1] = 0xB2;
+  machine.REGISTERS[0xA] = 0x07;
+  machine.REGISTERS[0xB] = 0x03;
+  decode(&machine, &instruction);
+
+  cr_expect(machine.REGISTERS[0xA] == 0x03, "Expected REGISTERS[0xA] to contain value 0x03, found 0x%02x", machine.REGISTERS[0xA]);
+
+  instruction[0] = 0x85;
+  instruction[1] = 0x62;
+  machine.REGISTERS[0x5] = 0x0e;
+  machine.REGISTERS[0x6] = 0x06;
+  decode(&machine, &instruction);
+
+  cr_expect(machine.REGISTERS[0x5] == 0x06, "Expected REGISTERS[0x5] to contain value 0x06, found 0x%02x", machine.REGISTERS[0x5]);
+}
