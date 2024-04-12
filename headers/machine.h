189777f0625a9d30147ed864998947933ae1e97d
Garrett Bodley <garrett.bodley@gmail.com>
2024-04-12 15:41:02 -0400

    Make all fields of Machine_s volatile.

--- headers/machine.h
+++ headers/machine.h
@@ -14,15 +14,15 @@
 #define SCREEN_REGISTER_COUNT 256 // 64 * 32 / 8
 
 struct Machine_s {
-  uint8_t SP;
-  uint16_t I;
-  uint16_t PC;
-  uint8_t DELAY_TIMER;
-  uint8_t SOUND_TIMER;
+  volatile uint8_t SP;
+  volatile uint16_t I;
+  volatile uint16_t PC;
+  volatile uint8_t DELAY_TIMER;
+  volatile uint8_t SOUND_TIMER;
 
-  uint8_t REGISTERS[REGISTER_COUNT];
-  uint16_t STACK[STACK_SIZE];
-  uint8_t MEMORY[MEM_SIZE];
+  volatile uint8_t REGISTERS[REGISTER_COUNT];
+  volatile uint16_t STACK[STACK_SIZE];
+  volatile uint8_t MEMORY[MEM_SIZE];
 
   #ifdef SDL
   SDL_Surface* SCREEN;
